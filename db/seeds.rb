e361d2b78225d5f309944524cf9206eb33cfffc1
Savina Aliaj savina.aliaj@gmail.com <savina.aliaj@gmail.com>
2020-04-06 17:58:14 +0200

    added some style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
 
-Ingredient.create(name: 'lemon')
-Ingredient.create(name: 'ice')
-Ingredient.create(name: 'mint leaves')
+Ingredient.create(name: 'Lemon')
+Ingredient.create(name: 'Ice')
+Ingredient.create(name: 'Mint leaves')
+Ingredient.create(name: 'Prosecco')
+Ingredient.create(name: 'Aperol')
+Ingredient.create(name: 'Gin')
+Ingredient.create(name: 'Sweet red Vermouth')
+Ingredient.create(name: 'Campari')
+Ingredient.create(name: 'White rum')
+Ingredient.create(name: 'Soda water')
+Ingredient.create(name: 'Lime Juice')
+Ingredient.create(name: 'Tequila')
 
+Cocktail.create(name: 'Negroni')
 Cocktail.create(name: 'Aperol Spritz')
 Cocktail.create(name: 'Mojito')
 Cocktail.create(name: 'Margarita')
-Cocktail.create(name: 'Sex On The Beach')
